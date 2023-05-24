8b0030f7a4e705c138af4d35309db62de1035c63
Ben Chidgey <ben@feedmypixel.com>
2023-05-24 14:28:17 +0100

    177683: Remove work from Template
    That is to be used in the Demo

--- src/app/router.js
+++ src/app/router.js
@@ -1,7 +1,6 @@
 import inert from '@hapi/inert'
 import { health } from '~/src/app/health'
 import { home } from '~/src/app/home'
-import { products } from '~/src/app/products'
 import { serveStaticFiles } from '~/src/common/helpers/serve-static-files'
 
 const router = {
@@ -9,7 +8,7 @@ const router = {
     name: 'router',
     register: async (server) => {
       await server.register([inert])
-      await server.register([health, home, products, serveStaticFiles])
+      await server.register([health, home, serveStaticFiles])
     }
   }
 }
