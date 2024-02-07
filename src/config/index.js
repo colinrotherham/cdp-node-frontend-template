d402498f7de47a969fad15b9a74ac22759e6ad9d
Ivar Abrahamsen <ivar.abrahamsen@defra.gov.uk>
2024-02-07 14:55:54 +0000

    Proxy helpers (#44)
    * Proxy helpers
    
    * formatting

--- src/config/index.js
+++ src/config/index.js
@@ -58,6 +58,20 @@ const config = convict({
     format: ['fatal', 'error', 'warn', 'info', 'debug', 'trace', 'silent'],
     default: 'info',
     env: 'LOG_LEVEL'
+  },
+  httpProxy: {
+    doc: 'HTTP Proxy',
+    format: String,
+    nullable: true,
+    default: null,
+    env: 'CDP_HTTP_PROXY'
+  },
+  httpsProxy: {
+    doc: 'HTTPS Proxy',
+    format: String,
+    nullable: true,
+    default: null,
+    env: 'CDP_HTTPS_PROXY'
   }
 })
 
