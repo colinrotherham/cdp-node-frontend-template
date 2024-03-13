10c59ef42f68a806a153801d1f99767f49d0087f
Ben Chidgey <ben@feedmypixel.com>
2024-03-13 15:44:12 +0000

    Remove breadcrumbs example from home

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -2,16 +2,7 @@ const homeController = {
   handler: (request, h) => {
     return h.view('home/index', {
       pageTitle: 'Home',
-      heading: 'Home',
-      breadcrumbs: [
-        {
-          text: 'Home',
-          href: '/'
-        },
-        {
-          text: 'Example'
-        }
-      ]
+      heading: 'Home'
     })
   }
 }
